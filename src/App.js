cce849d3a11807383ea0d006104d95b7298c3390
BaryYur <73163625+BaryYur@users.noreply.github.com>
2022-01-12 17:51:03 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,61 +1,88 @@
-import React, { useState } from 'react';
-
-// import { Button, Stack } from 'react-bootstrap';
-import Container from 'react-bootstrap/Container';
-import AddBudgetModal from './components/AddBudgetModal';
-import AddExpenseModal from './components/AddExpenseModal copy';
-import UncategorizedBudgetCard from './components/UncategorizedBudgetCard';
-
-import BudgetCard from './components/BudgetCard';
-import { useBudgets } from './contexts/BudgetContext';
-import TotalBudgetCard from './components/TotalBudgetCard';
+import { Button, Stack } from "react-bootstrap"
+import Container from "react-bootstrap/Container"
+import AddBudgetModal from "./components/AddBudgetModal"
+import AddExpenseModal from "./components/AddExpenseModal"
+import ViewExpensesModal from "./components/ViewExpensesModal"
+import BudgetCard from "./components/BudgetCard"
+import UncategorizedBudgetCard from "./components/UncategorizedBudgetCard"
+import TotalBudgetCard from "./components/TotalBudgetCard"
+import { useState } from "react"
+import { UNCATEGORIZED_BUDGET_ID, useBudgets } from "./contexts/BudgetsContext"
 
 function App() {
-  const [showAddBudgetModal, setShowAddBudgetModal] = useState(false);
-  const [showAddExpenseModal, setShowAddExpenseModal] = useState(false);
-  const [addExpenseModalBudgetId, setAddExpenseModalBudgetId] = useState();
-  const { budgets, getBudgetExpenses } = useBudgets();
+  const [showAddBudgetModal, setShowAddBudgetModal] = useState(false)
+  const [showAddExpenseModal, setShowAddExpenseModal] = useState(false)
+  const [viewExpensesModalBudgetId, setViewExpensesModalBudgetId] = useState()
+  const [addExpenseModalBudgetId, setAddExpenseModalBudgetId] = useState()
+  const { budgets, getBudgetExpenses } = useBudgets()
 
   function openAddExpenseModal(budgetId) {
-    setShowAddExpenseModal(true);
-    setAddExpenseModalBudgetId(budgetId);
+    setShowAddExpenseModal(true)
+    setAddExpenseModalBudgetId(budgetId)
   }
 
   return (
-    <React.Fragment>
-      <Container className='my-4'>
-        <div 
-          style={{ 
-            display: 'grid', 
-            gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))', 
-            gap: '1rem', 
-            alignItems: 'flex-start' 
+    <>
+      <Container className="my-4">
+        <Stack direction="horizontal" gap="2" className="mb-4">
+          <h1 className="me-auto">Budgets</h1>
+          <Button variant="primary" onClick={() => setShowAddBudgetModal(true)}>
+            Add Budget
+          </Button>
+          <Button variant="outline-primary" onClick={openAddExpenseModal}>
+            Add Expense
+          </Button>
+        </Stack>
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
+            gap: "1rem",
+            alignItems: "flex-start",
           }}
         >
-          {
-            budgets.map(budget => {
-              const amount = getBudgetExpenses(budget.id).reduce((total, expense) => total + expense.amount, 0);
-              return (
-                <BudgetCard 
-                  key={budget.id} 
-                  name={budget.name} 
-                  gray 
-                  amount={amount} 
-                  max={budget.max}
-                  onAddExpenseClick={() => openAddExpenseModal(budget.id)}
-                />
-                )
-            })
-          }
-          <UncategorizedBudgetCard />
+          {budgets.map(budget => {
+            const amount = getBudgetExpenses(budget.id).reduce(
+              (total, expense) => total + expense.amount,
+              0
+            )
+            return (
+              <BudgetCard
+                key={budget.id}
+                name={budget.name}
+                amount={amount}
+                max={budget.max}
+                onAddExpenseClick={() => openAddExpenseModal(budget.id)}
+                onViewExpensesClick={() =>
+                  setViewExpensesModalBudgetId(budget.id)
+                }
+              />
+            )
+          })}
+          <UncategorizedBudgetCard
+            onAddExpenseClick={openAddExpenseModal}
+            onViewExpensesClick={() =>
+              setViewExpensesModalBudgetId(UNCATEGORIZED_BUDGET_ID)
+            }
+          />
           <TotalBudgetCard />
-          <BudgetCard name='Entertaimant' amount={200} max={1000}></BudgetCard>
         </div>
       </Container>
-      <AddBudgetModal show={showAddBudgetModal} handleClose={() => setShowAddBudgetModal(false)} />
-      <AddExpenseModal defaultBudgetId={addExpenseModalBudgetId} show={showAddExpenseModal} handleClose={() => setShowAddExpenseModal(false)} />
-    </React.Fragment>
-  );
+      <AddBudgetModal
+        show={showAddBudgetModal}
+        handleClose={() => setShowAddBudgetModal(false)}
+      />
+      <AddExpenseModal
+        show={showAddExpenseModal}
+        defaultBudgetId={addExpenseModalBudgetId}
+        handleClose={() => setShowAddExpenseModal(false)}
+      />
+      <ViewExpensesModal
+        budgetId={viewExpensesModalBudgetId}
+        handleClose={() => setViewExpensesModalBudgetId()}
+      />
+    </>
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BudgetsProvider } from './contexts/BudgetContext';
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
+import "bootstrap/dist/css/bootstrap.min.css"
+import { BudgetsProvider } from "./contexts/BudgetsContext"
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,10 +10,5 @@ ReactDOM.render(
       <App />
     </BudgetsProvider>
   </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  document.getElementById("root")
+)
